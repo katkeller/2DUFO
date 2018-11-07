78f54a0e2ef0fee547d9d9d63ccb773a6e5111bd
katkeller <43050286+katkeller@users.noreply.github.com>
2018-11-06 19:51:40 -0600

    Created timer UI element and timer script

---
+++ 2DUFOSeries/Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private Text timerText;
+
+    [SerializeField]
+    private float timerStartTime;
+
+    private float timer;
+    private bool canCountDown = true;
+    private bool doOnce = false;
+
+    private void Start()
+    {
+        timer = timerStartTime;
+    }
+
+    void Update()
+    {
+        if (timer >= 0.0f && canCountDown)
+        {
+            timer -= Time.deltaTime;
+            timerText.text = timer.ToString("F");
+        }
+        else if (timer<= 0.0f && !doOnce)
+        {
+            canCountDown = false;
+            doOnce = true;
+            timerText.text = "0.00";
+            timer = 0.0f;
+        }
+    }
+}
